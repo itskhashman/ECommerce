e1688b9d38559427499d6e3cf01ecc07e5259889
itskhashman <khashmanomar22@gmail.com>
2026-05-06 11:10:31 +0300

    Enhace the Base Entities Structure by adding the BaseMetadata Entity

---
+++ ECommerce.Domain/Entities/Base/AuditableEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Domain.Entities.Base
+{
+    public class AuditableEntity
+    {
+        [Required]
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        [Required]
+        public int ModifiedBy { get; set; }
+        public DateTime? ModifiedAt { get; set; } = DateTime.UtcNow;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
--- ECommerce.Domain/Entities/Base/BaseEntity.cs
+++ ECommerce.Domain/Entities/Base/BaseEntity.cs
@@ -2,17 +2,9 @@
 
 namespace ECommerce.Domain.Entities.Base
 {
-    public class BaseEntity
+    public class BaseEntity : AuditableEntity
     {
         [Key]
         public int Id { get; set; }
-        [EmailAddress]
-        [Required]
-        public string CreatedBy { get; set; } = null!;
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        [EmailAddress]
-        public string? ModifiedBy { get; set; }
-        public DateTime? ModifiedAt { get; set; } = DateTime.UtcNow;
-        public bool IsDeleted { get; set; } = false;
     }
 }
