095211542953e8c47db8500d7d3350cafc4ae897
itskhashman <khashmanomar22@gmail.com>
2026-06-04 11:44:04 +0300

    Add Users management feature for Admin role

---
+++ ECommerce.Web/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
